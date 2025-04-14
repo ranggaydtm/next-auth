3c4355410807585d8fce687fcf9e58d79dc97edc
ranggaydtm <ranggay67@gmail.com>
2025-04-14 16:54:57 +0700

    Initial commit

---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+const LoginPage = () => {
+  return (
+    <div className="w-full flex items-center justify-center">
+      <div className="text-white">Login</div>
+    </div>
+  );
+};
+
+export default LoginPage;
